d44bcddeef9e261709bcc9730a921a7cf1b4f4c6
Braian Corrêa <00.braian.dev@gmail.com>
2021-05-11 01:08:32 -0300

    [Component] Bird

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
 import Bird from "./components/Bird";
 
 export default function App() {
+  const screenWidth = Dimensions.get("screen").width;
+  const screenHeight = Dimensions.get("screen").height;
+  const birdLeft = screenWidth / 2;
+  const [BirdBottom, setBirdBottom] = useState(screenHeight / 2);
+  const gravity = 3;
+  let gameTimerId;
+
+  // start bird falling
+  useEffect(() => {
+    if (BirdBottom > 0) {
+      gameTimerId = setInterval(() => {
+        setBirdBottom((BirdBottom) => BirdBottom - gravity);
+      }, 30);
+    }
+
+    return () => {
+      clearInterval(gameTimerId);
+    };
+  }, [BirdBottom]);
+
   return (
     <View style={styles.container}>
-      <Bird />
+      <Bird birdBottom={BirdBottom} birdLeft={birdLeft} />
     </View>
   );
 }
--- components/Bird.js
+++ components/Bird.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import { View } from 'react-native';
+import React from "react";
+import { View } from "react-native";
 
-function Bird() {
-    return (
-        <View style={{
-            position: 'absolute',
-            backgroundColor: 'blue',
-            width: 50,
-            height: 60,
-            
-        }}>      
-        </View>
-    )
-}
+const Bird = ({ birdBottom, birdLeft }) => {
+  const birdWidth = 50;
+  const birdHeight = 60;
 
-export default Bird
+  return (
+    <View
+      style={{
+        position: "absolute",
+        backgroundColor: "blue",
+        width: birdWidth,
+        height: birdHeight,
+        left: birdLeft - birdWidth / 2,
+        bottom: birdBottom - birdHeight / 2,
+      }}
+    ></View>
+  );
+};
+
+export default Bird;
