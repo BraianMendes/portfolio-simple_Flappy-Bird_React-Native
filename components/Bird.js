419817ad43a3a152aafc727ad19ecc879a562f9e
Braian Corrêa <00.braian.dev@gmail.com>
2021-05-11 00:56:29 -0300

    [Start] Start Project

---
+++ components/Bird.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View } from 'react-native';
+
+function Bird() {
+    return (
+        <View style={{
+            position: 'absolute',
+            backgroundColor: 'blue',
+            width: 50,
+            height: 60,
+            
+        }}>      
+        </View>
+    )
+}
+
+export default Bird
