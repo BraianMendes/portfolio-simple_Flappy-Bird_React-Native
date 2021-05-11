1ceb47986f850d29e40367c51ff22d152aac4c77
Braian Corrêa <00.braian.dev@gmail.com>
2021-05-11 01:22:07 -0300

    [Component] Obstacle

--- App.js
+++ App.js
@@ -1,14 +1,34 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View, Dimensions } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  TouchableWithoutFeedback,
+} from "react-native";
 import Bird from "./components/Bird";
+import Obstacles from "./components/Obstacles";
 
 export default function App() {
   const screenWidth = Dimensions.get("screen").width;
   const screenHeight = Dimensions.get("screen").height;
   const birdLeft = screenWidth / 2;
   const [BirdBottom, setBirdBottom] = useState(screenHeight / 2);
+  const [obstaclesLeft, setObstaclesLeft] = useState(screenWidth);
+  const [obstaclesLeftTwo, setObstaclesLeftTwo] = useState(
+    screenWidth + screenWidth / 2 + 30
+  );
+  const [obstaclesNegHeight, setObstaclesNegHeight] = useState(0);
+  const [obstaclesNegHeightTwo, setObstaclesNegHeightTwo] = useState(0);
+  const [isGameOver, setIsGameOver] = useState(false);
+  const [score, setScore] = useState(0);
   const gravity = 3;
+  let obstacleWidth = 60;
+  let obstacleHeight = 300;
+  let gap = 200;
   let gameTimerId;
+  let obstaclesTimerId;
+  let obstaclesTimerIdTwo;
 
   // start bird falling
   useEffect(() => {
@@ -23,9 +43,30 @@ export default function App() {
     };
   }, [BirdBottom]);
 
+  // start first obstacle
+  useEffect(() => {
+    if (obstaclesLeft > 0) {
+      obstaclesTimerId = setInterval(() => {
+        setObstaclesLeft((obstaclesLeft) => obstaclesLeft - 5);
+      }, 30);
+    }
+
+    return () => {
+      clearInterval(obstaclesTimerId);
+    };
+  }, [obstaclesLeft]);
+
   return (
     <View style={styles.container}>
       <Bird birdBottom={BirdBottom} birdLeft={birdLeft} />
+      <Obstacles
+        color={"green"}
+        obstacleWidth={obstacleWidth}
+        obstacleHeight={obstacleHeight}
+        randomBottom={obstaclesNegHeight}
+        gap={gap}
+        obstaclesLeft={obstaclesLeft}
+      />
     </View>
   );
 }
